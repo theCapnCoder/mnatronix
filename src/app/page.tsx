8d05e10a21615c25a318877f35712a94d4ff2191
PavelM <myagkiypavel@gmail.com>
2024-12-13 14:12:04 +0200

    feat: add ru.json for i18n and config i18n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { useTranslations } from "next-intl";
+
 export default function Home() {
+  const t = useTranslations("header");
+
   return (
     <div>
       <h2>Home</h2>
+      <p>{t("nav.about_us")}</p>
+
     </div>
   );
 }
