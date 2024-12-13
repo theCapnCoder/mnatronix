a44c06818944242ca46b51ab09f3538d3ed2bb26
PavelM <myagkiypavel@gmail.com>
2024-12-13 13:27:46 +0200

    feat: add clxs, svgr, swiper, tailwind eslinter, i18n, colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,34 @@
 import type { Config } from "tailwindcss";
+import fluid, { extract, fontSize } from "fluid-tailwind";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: {
+    files: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    extract,
+  },
   theme: {
+    fontSize,
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "black": "282828",
+        "black-1": "100F0F",
+        "bg": "F7F7F7",
+        "grey": "EAEAEA",
+        "grey-1": "DFDFDF",
+        "grey-2": "A2A2A2",
+        "grey-3": "A0A0A0",
+        "grey-4": "3E3B3B",
       },
     },
+    fluid: () => ({
+      defaultScreens: ["20rem", "90rem"],
+    }),
   },
-  plugins: [],
+  plugins: [fluid],
 } satisfies Config;
