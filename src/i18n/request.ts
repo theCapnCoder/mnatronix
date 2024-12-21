b3d1949f72ccd05fc10acb3871967ce10f41866c
PavelM <myagkiypavel@gmail.com>
2024-12-21 17:34:37 +0200

    feat: config i18n

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,7 +1,13 @@
 import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
 
-export default getRequestConfig(async () => {
-  const locale = "ru";
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
 
   return {
     locale,
